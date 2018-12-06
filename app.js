f371d8b79a1dc19de4671b26a91581d56229bcee
dieple <dieple@kootoro.com>
2018-12-07 00:47:20 +0700

    add function update master content

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
-app.get("/",(req, res)=> res.send("Hello, this is simple app node js"));
+const path = require('path');
+const bodyParser = require('body-parser');
+app.set('view engine', 'ejs'); //view engine
+app.set('views', 'views'); //views
+app.use(bodyParser.urlencoded({extended: false})); //include bodyparser
+app.use(express.static(path.join(__dirname, 'public')));
+app.get("/",(req, res)=> res.render('index'));
+
 app.listen(4000, ()=>{ console.log("Running port 4000...")});
